80d51e341c4fa4597ef9ba9352d7221d8dcb610f
Matteo Ugliotti <matteougliotti@gmail.com>
2026-02-11 17:15:55 -0500

    Update time_stepping.py

--- upstream_jax_cfd/spectral/time_stepping.py
+++ upstream_jax_cfd/spectral/time_stepping.py
@@ -1,4 +1,5 @@
 # Copyright 2021 Google LLC
+# Modifications Copyright 2026 Mateo Reynoso
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
